78d16f96ad549b1f3192f3d910d8cd16558afb5b
Andrew Dodson <sonofdod@gmail.com>
2015-09-04 13:54:30 +0100

    update

---
+++ polyfill/requestAnimationFrame.js
@@ -0,0 +1,11 @@
+// requestAnimationFrame polyfill
+window.requestAnimationFrame = (function(){
+	return window.requestAnimationFrame ||
+		window.webkitRequestAnimationFrame ||
+		window.mozRequestAnimationFrame ||
+		window.oRequestAnimationFrame ||
+		window.msRequestAnimationFrame ||
+		function (callback) {
+			window.setTimeout(callback, 1000 / 60);
+		};
+})();
