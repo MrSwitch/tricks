000ff6f7f394673505a5d2352b4114d7444ca84b
Andrew Dodson <sonofdod@gmail.com>
2015-09-13 23:37:11 +0100

    Switch to localhost file-server

--- gulpfile.js
+++ gulpfile.js
@@ -2,27 +2,15 @@ var gulp = require('gulp');
 var mochaPhantomJS = require('gulp-mocha-phantomjs');
 var path = require('path');
 
-var http = require('http');
-var serveStatic = require('serve-static');
-var finalhandler = require('finalhandler');
 
-// Serve up public/ftp folder
-var serve = serveStatic('./');
 var port = 8080;
-
-// Create server
-http.createServer(function(req, res){
-	req.method = 'GET';
-	var done = finalhandler(req, res);
-	serve(req, res, done);
-}).listen(port);
-
+require('localhost')('./').listen(port);
 console.log('Listening on port ' + port);
 
 
 gulp.task('test', function () {
     var stream = mochaPhantomJS();
-    stream.write({path: 'http://localhost:8080/specs/index.html', reporter:'spec'});
+    stream.write({path: 'http://localhost:' + port + '/specs/index.html', reporter:'spec'});
     stream.end();
     return stream;
 });
