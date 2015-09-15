a54f4b798a8af7d892d075fc7f93af1991961f8a
Andrew Dodson <sonofdod@gmail.com>
2015-09-15 22:06:21 +0100

    Refactor GA code

--- services/googleanalytics.js
+++ services/googleanalytics.js
@@ -1,9 +1,9 @@
 import getScript from '../http/getScript.js';
 
-export default function (tracking) {
+export default (tracking) => {
 	window._gaq = window._gaq || [];
 	_gaq.push(['_setAccount', tracking]);
 	_gaq.push(['_trackPageview']);
 
 	getScript(('https:' == document.location.protocol ? 'https://ssl' : 'http://www') + '.google-analytics.com/ga.js');
-}
+};
