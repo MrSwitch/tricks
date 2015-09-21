0150ebc21145b257a8ecf63a73f8df6438b8df62
Andrew Dodson <sonofdod@gmail.com>
2015-09-21 16:12:26 +0100

    Gulp self building test index

--- gulpfile.js
+++ gulpfile.js
@@ -1,7 +1,7 @@
 var gulp = require('gulp');
 var mochaPhantomJS = require('gulp-mocha-phantomjs');
+var fs = require('fs');
 var path = require('path');
-
 
 var port = 8080;
 var localhost = require('localhost')('./');
@@ -16,8 +16,24 @@ gulp.task('test', function () {
     return stream;
 });
 
+gulp.task('test_index', function () {
+	var root = __dirname.replace(/\\/g, '/') + '/specs/';
+
+	// for the given files in the test directory, create an index
+	return gulp.src(['specs/*/*.js', '!specs/components{,/**}'], function(err, files){
+		// Write line to the index file
+		var index = files.filter(function(name){
+			// shouldn't have to do this if the glob '!specs/components{,/**}' worked, urgh!
+			return !name.match('/components/');
+		}).map(function(name) {
+			return "import '" + name.replace(root, './') + "';";
+		});
+		fs.writeFileSync('specs/index.js', index.join("\n"));
+	});
+});
+
 gulp.task('watch', function () {
-	gulp.watch(['**/*.js','!node_modules/**/*','!specs/components/**/*'], {interval: 500}, ['test']);
+	gulp.watch(['**/*.js','!node_modules/**/*','!specs/components/**/*'], {interval: 500}, ['test_index', 'test']);
 });
 
 gulp.task('close', ['test'], function () {
--- specs/index.js
+++ specs/index.js
@@ -1,12 +1,10 @@
-// Load in the specs
 import './animation/timing.js';
-
-import './dom/attr.js';
 import './dom/append.js';
+import './dom/attr.js';
 import './dom/createElement.js';
-import './dom/each.js';
 import './dom/domInstance.js';
 import './dom/domToJSON.js';
+import './dom/each.js';
 import './dom/hiddenFrame.js';
 import './dom/inviewport.js';
 import './dom/isDom.js';
@@ -14,21 +12,17 @@ import './dom/matches.js';
 import './dom/objectfit.js';
 import './dom/prepend.js';
 import './dom/until.js';
-
 import './events/createEvent.js';
 import './events/emit.js';
 import './events/globalCallback.js';
 import './events/on.js';
 import './events/visible.js';
-
 import './helper/popup.js';
 import './helper/store.js';
-
-import './http/getScript.js';
-import './http/xhr.js';
-import './http/jsonp.js';
 import './http/formpost.js';
-
+import './http/getScript.js';
+import './http/jsonp.js';
+import './http/xhr.js';
 import './object/args.js';
 import './object/clone.js';
 import './object/diff.js';
@@ -43,18 +37,14 @@ import './object/toArray.js';
 import './object/toBlob.js';
 import './object/unique.js';
 import './object/url.js';
-
 import './string/createUrl.js';
-import './string/param.js';
 import './string/extract.js';
+import './string/param.js';
 import './string/queryparse.js';
 import './string/random.js';
 import './string/toXML.js';
 import './string/trim.js';
-
 import './support/browser.js';
 import './support/canvasToBlob.js';
-import './support/requestAnimationFrame.js';
 import './support/index.js';
-
-
+import './support/requestAnimationFrame.js';
