9a3c43c4af56ef47cd3c859bca1f29591f18f3e7
Andrew Dodson <sonofdod@gmail.com>
2015-11-12 20:31:49 +0000

    Spec window/close

---
+++ window/close.js
@@ -0,0 +1,22 @@
+// Close a window
+export default (window) => {
+
+	// Is this window within an Iframe?
+	if (window.frameElement) {
+		window.parent.document.body.removeChild(window.frameElement);
+	}
+	else {
+		// Close this current window
+		try {
+			window.close();
+		}
+		catch (e) {}
+
+		// IOS bug wont let us close a popup if still loading
+		if (window.addEventListener) {
+			window.addEventListener('load', function() {
+				window.close();
+			});
+		}
+	}
+};
