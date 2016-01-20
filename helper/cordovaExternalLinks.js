515126fe96b1ad1e033137df35551cf92d34906b
Andrew Dodson <sonofdod@gmail.com>
2016-01-20 20:52:43 +0000

    Add fix for broken links in Cordova

---
+++ helper/cordovaExternalLinks.js
@@ -0,0 +1,21 @@
+import delegate from '../events/delegate';
+import cordova from '../support/cordova';
+
+export default (root = document) => {
+	if (!cordova) {
+		return false;
+	}
+	else {
+		// Enable event delegation to fix anchor elements
+		delegate('a', 'click', (e) => {
+
+			let target = e.delegateTarget;
+
+			// Check this is a valid external URL...
+			if (target.href && target.href.match(/^https?:\/\//)) {
+				e.preventDefault();
+				window.open(target.href, '_system');
+			}
+		}, root);
+	}
+};
