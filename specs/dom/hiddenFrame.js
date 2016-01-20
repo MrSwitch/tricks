9c956def94846233ad9493f380ffea012933c7a4
Andrew Dodson <sonofdod@gmail.com>
2016-01-20 15:54:55 +0000

    Spec improve robustness of dom/hiddenFrame clearup

--- specs/dom/hiddenFrame.js
+++ specs/dom/hiddenFrame.js
@@ -2,15 +2,18 @@ import hiddenFrame from 'dom/hiddenFrame.js';
 
 describe('dom/hiddenFrame', () => {
 
-	it('should create a new hidden iframe element and append it to the document body', () => {
-		var src = 'about:blank';
-		var elm = hiddenFrame(src);
-		expect(elm.parentNode).to.eql(document.body);
-		expect(elm.tagName).to.equal('IFRAME');
-		expect(elm.src).to.equal(src);
+	let elm = null;
 
+	afterEach(() => {
 		// Clean up
 		document.body.removeChild(elm);
 	});
 
+	it('should create a new hidden iframe element and append it to the document body', () => {
+		var src = 'about:blank';
+		elm = hiddenFrame(src);
+		expect(elm.parentNode).to.eql(document.body);
+		expect(elm.tagName).to.equal('IFRAME');
+		expect(elm.src).to.equal(src);
+	});
 });
