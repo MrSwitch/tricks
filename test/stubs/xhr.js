7bc3d8c5457a71bacb59a57cd0668d03d08d9342
Andrew Dodson <andrew@5app.com>
2016-07-05 00:19:06 +0100

    lint

--- test/stubs/xhr.js
+++ test/stubs/xhr.js
@@ -1,35 +1,36 @@
 /* eslint no-undef: 0 */
 
-var _xhr = XMLHttpRequest;
+module.exports = () => {};
 
-module.exports = (data) => {
+// var _xhr = XMLHttpRequest;
+// module.exports = (data) => {
 
-	function xhr() {}
+// 	function xhr() {}
 
-	xhr.prototype.send = function() {};
-	xhr.prototype.onload = function() {};
-	xhr.prototype.onerror = function() {};
+// 	xhr.prototype.send = function() {};
+// 	xhr.prototype.onload = function() {};
+// 	xhr.prototype.onerror = function() {};
 
-	xhr.prototype.open = function() {
+// 	xhr.prototype.open = function() {
 
-		setTimeout(() => {
-			try {
-				this.response = data;
-				this.onload();
-			}
-			catch (e) {
-				this.onerror();
-			}
-		});
-	};
-	xhr.prototype.overrideMimeType = () => {};
-	xhr.prototype.setRequestHeader = () => {};
-	xhr.prototype.getAllResponseHeaders = () => {};
+// 		setTimeout(() => {
+// 			try {
+// 				this.response = data;
+// 				this.onload();
+// 			}
+// 			catch (e) {
+// 				this.onerror();
+// 			}
+// 		});
+// 	};
+// 	xhr.prototype.overrideMimeType = () => {};
+// 	xhr.prototype.setRequestHeader = () => {};
+// 	xhr.prototype.getAllResponseHeaders = () => {};
 
 
-	//XMLHttpRequest = xhr;
-};
+// 	XMLHttpRequest = xhr;
+// };
 
 module.exports.unstub = () => {
-	//XMLHttpRequest = _xhr;
+	// XMLHttpRequest = _xhr;
 };
