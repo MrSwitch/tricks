11f4c6f6651be807b187df2db3fe51865001c165
Andrew Dodson <andrew@5app.com>
2025-07-31 11:52:36 +0100

    feat(loadScript): add browser http loadScript, docs

--- browser/http/loadScript.js
+++ browser/http/loadScript.js
@@ -1,6 +1,16 @@
 import createElement from '../../dom/createElement.js';
 import createEvent from '../../events/createEvent.js';
 
+/**
+ * Load a script from a URL and return a promise that resolves when the script has loaded
+ * @param {string} url - An absolute or relative URL to the script to load.
+ * @param {number} [timeout=0] - Optional timeout in milliseconds. If 0 no timeout is applied.
+ * @returns {Promise<HTMLScriptElement>} A promise that resolves with the script element when loaded, or rejects on error.
+ * @throws {Error<Event>} - Throws an error if the script fails to load or times out.
+ * @example <caption>Loads a script into the page</caption>
+ * await loadScript('https://example.com/script.js') // Load a remote script with a 5 second timeout
+ * await loadScript('./local.js', 5000) // Load a local script with a 5 second timeout
+ */
 export default function loadScript(url, timeout = 0) {
 
 	return new Promise((resolve, reject) => {
